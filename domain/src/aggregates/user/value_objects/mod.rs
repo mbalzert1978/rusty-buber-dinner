4bde5473162a7c52cabcf34a2001dca41dfa03e7
Markus Iorio <mbalzert@gmail.com>
2024-06-18 21:10:06 +0200

    folders and structures

---
+++ domain/src/aggregates/user/value_objects/mod.rs
@@ -0,0 +1,3 @@
+pub(crate) mod email;
+pub(crate) mod password;
+pub(crate) mod password_hash;
--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -1,4 +1,7 @@
 mod error;
 mod prelude;
+mod shared;
+
+mod aggregates;
 
 pub use error::DomainError;
